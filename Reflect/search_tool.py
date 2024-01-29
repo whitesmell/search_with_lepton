9826937b5c2cba88aaed1d19b79b19871ff903ac
Jihua Peng <jihua.peng@gmail.com>
2024-01-29 23:32:06 +0800

    add file

---
+++ Reflect/search_tool.py
@@ -0,0 +1,126 @@
+import concurrent.futures
+import glob
+import json
+import os
+import re
+import threading
+import requests
+import traceback
+from typing import Annotated, List, Generator, Optional
+
+from fastapi import HTTPException
+from fastapi.responses import HTMLResponse, StreamingResponse, RedirectResponse
+import httpx
+from loguru import logger
+
+import leptonai
+from leptonai import Client
+from leptonai.kv import KV
+from leptonai.photon import Photon, StaticFiles
+from leptonai.photon.types import to_bool
+from leptonai.api.workspace import WorkspaceInfoLocalRecord
+from leptonai.util import tool
+
+################################################################################
+# Constant values for the RAG model.
+################################################################################
+
+# Search engine related. You don't really need to change this.
+BING_SEARCH_V7_ENDPOINT = "https://api.bing.microsoft.com/v7.0/search"
+BING_MKT = "en-US"
+GOOGLE_SEARCH_ENDPOINT = "https://customsearch.googleapis.com/customsearch/v1"
+SERPER_SEARCH_ENDPOINT = "https://google.serper.dev/search"
+
+# Specify the number of references from the search engine you want to use.
+# 8 is usually a good number.
+REFERENCE_COUNT = 8
+
+# Specify the default timeout for the search engine. If the search engine
+# does not respond within this time, we will return an error.
+DEFAULT_SEARCH_ENGINE_TIMEOUT = 5
+
+
+def search_with_bing(query: str, subscription_key: str):
+    """
+    Search with bing and return the contexts.
+    """
+    params = {"q": query, "mkt": BING_MKT}
+    response = requests.get(
+        BING_SEARCH_V7_ENDPOINT,
+        headers={"Ocp-Apim-Subscription-Key": subscription_key},
+        params=params,
+        timeout=DEFAULT_SEARCH_ENGINE_TIMEOUT,
+    )
+    if not response.ok:
+        logger.error(f"{response.status_code} {response.text}")
+        raise HTTPException(response.status_code, "Search engine error.")
+    json_content = response.json()
+    try:
+        contexts = json_content["webPages"]["value"][:REFERENCE_COUNT]
+    except KeyError:
+        logger.error(f"Error encountered: {json_content}")
+        return []
+    return contexts
+
+
+def search_with_google(query: str, subscription_key: str, cx: str):
+    """
+    Search with google and return the contexts.
+    """
+    params = {
+        "key": subscription_key,
+        "cx": cx,
+        "q": query,
+        "num": REFERENCE_COUNT,
+    }
+    response = requests.get(
+        GOOGLE_SEARCH_ENDPOINT, params=params, timeout=DEFAULT_SEARCH_ENGINE_TIMEOUT
+    )
+    if not response.ok:
+        logger.error(f"{response.status_code} {response.text}")
+        raise HTTPException(response.status_code, "Search engine error.")
+    json_content = response.json()
+    try:
+        contexts = json_content["items"][:REFERENCE_COUNT]
+    except KeyError:
+        logger.error(f"Error encountered: {json_content}")
+        return []
+    return contexts
+
+
+def search_with_serper(query: str, subscription_key: str):
+    """
+    Search with serper and return the contexts.
+    """
+    payload = json.dumps({
+        "q": query,
+        "num": (
+            REFERENCE_COUNT
+            if REFERENCE_COUNT % 10 == 0
+            else (REFERENCE_COUNT // 10 + 1) * 10
+        ),
+    })
+    headers = {"X-API-KEY": subscription_key, "Content-Type": "application/json"}
+    logger.info(
+        f"{payload} {headers} {subscription_key} {query} {SERPER_SEARCH_ENDPOINT}"
+    )
+    response = requests.post(
+        SERPER_SEARCH_ENDPOINT,
+        headers=headers,
+        data=payload,
+        timeout=DEFAULT_SEARCH_ENGINE_TIMEOUT,
+    )
+    if not response.ok:
+        logger.error(f"{response.status_code} {response.text}")
+        raise HTTPException(response.status_code, "Search engine error.")
+    json_content = response.json()
+    try:
+        # convert to the same format as bing/google
+        contexts = [
+            {"name": c["title"], "url": c["link"], "snippet": c["snippet"]}
+            for c in json_content["organic"][:REFERENCE_COUNT]
+        ]
+    except KeyError:
+        logger.error(f"Error encountered: {json_content}")
+        return []
+    return contexts
